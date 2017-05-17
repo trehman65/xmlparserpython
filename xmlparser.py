62e58e9a5dd75685009517c4702f125e21b34c6b
Talha Rehman <14beetrehman@seecs.edu.pk>
2017-05-17 19:44:19 +0500

    dev: final code, plots boxes on image

--- xmlparser.py
+++ xmlparser.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from xml.dom import minidom
 import io
 import os
@@ -11,13 +12,17 @@ import time
 import sys
 import json
 import os.path
+import cv2
 
 outputLines=[]
 finalOutputJson=sys.argv[2]
 xmldoc = minidom.parse(sys.argv[1])
+# img = cv2.imread('/Users/talha/Documents/currentworkspace/xmlomni/xml2jsonLineLevel/1.jpeg')
+# finalOutputImage="/Users/talha/Documents/currentworkspace/xmlomni/xml2jsonLineLevel/1-seg.jpeg"
 
 lines = xmldoc.getElementsByTagName('ln')
 lenLines = len(lines)
+factor=300/1440
 
 count=1;
 
@@ -33,11 +38,16 @@ for s in lines:
 		thisWord=t.childNodes[0].nodeValue
 		if(thisWord!=None):
 			thisLine = thisLine + thisWord + " "
+
+	l=float(s.attributes['l'].value)*factor
+	r=float(s.attributes['r'].value)*factor
+	t=float(s.attributes['t'].value)*factor
+	b=float(s.attributes['b'].value)*factor
 	
-	l=s.attributes['l'].value
-	r=s.attributes['r'].value
-	t=s.attributes['t'].value
-	b=s.attributes['b'].value
+	#cv2.rectangle(img,l, t, r, b, (0, 0, 255), 3)
+	
+	#cv2.rectangle(img,(int(l), int(t)), (int(r), int(b)), (0, 0, 255), 3)
+	#cv2.rectangle(img,(154,1147), (1546,1277), (0, 0, 255), 3)
 	
 	temp={}
 	temp["id"]=count
@@ -50,6 +60,9 @@ for s in lines:
 	outputLines.append(temp)
 	print thisLine
 	count=count+1
+
+cv2.imwrite(finalOutputImage,img)
+
 
 with open(finalOutputJson, 'w') as outfile:  
         json.dump(outputLines, outfile)
